fc9a59bf4cab68848f84ad45a1ca8322dcd404af
Eiryyy <xxx.eiryklav@gmail.com>
2016-03-31 01:14:32 +0900

    単純に録画する

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # agqr-recorder
 A streaming recorder from www.agqr.jp for node.js
+
+インスパイア元: [https://gist.github.com/ybenjo/9904543](https://gist.github.com/ybenjo/9904543)
+
+## Dependency
+rtmpdump
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import 'babel-polyfill';
+
+import rtmpdump from 'rtmpdump';
+import fs from 'fs';
+import schedule from '../schedule.json';
+import 'date-utils';
+
+
+const main = () => {
+	let time = new Date();
+	time.addMinutes(5);
+
+	schedule.programs.forEach((program) => {
+		if (Date.getDayNumberFromName(program.wday) === time.getDay()) {
+			const start = new Date( `${time.toYMD()} ${program.time}` );
+			const end = start.clone().addMinutes(program.length);
+			if (time.between(start, end)) {
+				record(program, time);
+			}
+		}
+	});
+};
+
+const waitSeconds = (seconds) => {
+	return new Promise((done) => {
+		setTimeout(() => {
+			done();
+		}, seconds * 1000);
+	});
+};
+
+const record = async (program, date) => {
+	await waitSeconds(45);
+	const stream = rtmpdump.createStream({
+		rtmp: 'rtmp://fms-base1.mitene.ad.jp/agqr/aandg22',
+		stop: program.length * 60,
+		v: null
+	});
+	stream.on('error', () => {
+		// 接続時に必ずエラーが起きるので何もしない
+	});
+	stream.pipe(fs.createWriteStream(`out/${program.title}-${date.toYMD()}.flv`));
+};
+
+main();
